10cb75b2bcb024c7d41892696b516546a9df3a75
Ken Nagamine <7955031+nag8@users.noreply.github.com>
2020-08-07 23:07:15 +0900

    シート修正

--- src/sheet.py
+++ src/sheet.py
@@ -38,6 +38,34 @@ def uploadSpreadsheet():
             if ('【関西】クローズ理由_' + dateStr) in file:
                 closeList = getList(config['CSV']['DLPATH'] + file)
                 logList.insert(0, ['upload成功_' + file])
+
+            if ('企業管理データ(当月アプローチ企業)_' + dateStr) in file:
+                approachList = getList(config['CSV']['DLPATH'] + file)
+                logList.insert(0, ['upload成功_' + file])
+                
+            if ('企業管理データ(契約締結)_' + dateStr) in file:
+                policyList = getList(config['CSV']['DLPATH'] + file)
+                logList.insert(0, ['upload成功_' + file])
+                
+            if ('企業管理データ(リーガルチェック)_' + dateStr) in file:
+                regalList = getList(config['CSV']['DLPATH'] + file)
+                logList.insert(0, ['upload成功_' + file])
+                
+            if ('企業管理データ(電話商談)_' + dateStr) in file:
+                callList = getList(config['CSV']['DLPATH'] + file)
+                logList.insert(0, ['upload成功_' + file])
+                
+            if ('企業管理データ(オフライン商談)_' + dateStr) in file:
+                offLineList = getList(config['CSV']['DLPATH'] + file)
+                logList.insert(0, ['upload成功_' + file])
+                
+            if ('企業管理データ(オンライン商談)_' + dateStr) in file:
+                onLineList = getList(config['CSV']['DLPATH'] + file)
+                logList.insert(0, ['upload成功_' + file])
+                
+            if ('企業管理データ(アポ獲得)_' + dateStr) in file:
+                appoList = getList(config['CSV']['DLPATH'] + file)
+                logList.insert(0, ['upload成功_' + file])
                 
         for file in fList:
             if ('受講生フェーズ（2019年）_' + dateStr) in file:
@@ -50,6 +78,13 @@ def uploadSpreadsheet():
         googleUtil.uploadData(recruitList, config['GOOGLE']['SHEET_RECRUIT'])
         googleUtil.uploadData(closeList, config['GOOGLE']['SHEET_CLOSE'])
         googleUtil.uploadData(logList, config['GOOGLE']['SHEET_LOG'])
+        googleUtil.uploadData(approachList, config['GOOGLE']['SHEET_APPROACH'])
+        googleUtil.uploadData(policyList, config['GOOGLE']['SHEET_POLICY'])
+        googleUtil.uploadData(regalList, config['GOOGLE']['SHEET_REGAL'])
+        googleUtil.uploadData(callList, config['GOOGLE']['SHEET_CALL'])
+        googleUtil.uploadData(offLineList, config['GOOGLE']['SHEET_OFFLINE'])
+        googleUtil.uploadData(onLineList, config['GOOGLE']['SHEET_ONLINE'])
+        googleUtil.uploadData(appoList, config['GOOGLE']['SHEET_APPO'])
     
     except Exception as e:
         logList = [['error! ' + dateStr]]

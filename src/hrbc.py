d8000fba8e0741f370e8d86816827efd722fe06d
Ken Nagamine <7955031+nag8@users.noreply.github.com>
2020-08-07 18:02:49 +0900

    hrbc設定

--- src/hrbc.py
+++ src/hrbc.py
@@ -11,7 +11,7 @@ def downloadCSV():
     config = util.getConfig()
     
     options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(executable_path=config['CHROME']['PATH'], options=options)
+    driver = webdriver.Chrome(options=options)
 
     try:
         driver.get('https://hrbc-jp.porterscloud.com/common/navigation')
@@ -27,30 +27,26 @@ def downloadCSV():
         
         time.sleep(5)
         
-        # レポートダウンロード
-        driver.get(config['HRBC']['URL_1'])
-        time.sleep(5)
-        driver.find_element_by_class_name('download').click()
-        time.sleep(3)
-        
-        driver.get(config['HRBC']['URL_2'])
-        time.sleep(5)
-        driver.find_element_by_class_name('download').click()
-        time.sleep(3)
-        
-        driver.get(config['HRBC']['URL_3'])
-        time.sleep(20)
-        driver.find_element_by_class_name('download').click()
-        time.sleep(10)
-
-        driver.get(config['HRBC']['URL_4'])
-        time.sleep(10)
-        driver.find_element_by_class_name('download').click()
-        
-        time.sleep(10)
+        download(config['HRBC']['URL_1'])
+        download(config['HRBC']['URL_2'])
+        download(config['HRBC']['URL_3'])
+        download(config['HRBC']['URL_4'])
+        download(config['HRBC']['URL_5'])
+        download(config['HRBC']['URL_6'])
+        download(config['HRBC']['URL_7'])
+        download(config['HRBC']['URL_8'])
+        download(config['HRBC']['URL_9'])
+        download(config['HRBC']['URL_10'])
         
     except Exception as e:
         logging.error(traceback.format_exc())
 
     finally:
         driver.quit()
+
+# レポートダウンロード
+def download(url):
+    driver.get(url)
+    time.sleep(10)
+    driver.find_element_by_class_name('download').click()
+    time.sleep(10)
